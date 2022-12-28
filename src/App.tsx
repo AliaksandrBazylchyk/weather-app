dc5d3c28f70ab4376697fd773de389162634e027
Aliaksandr Bazylchyk <bazylchyk.a@gmail.com>
2022-12-28 22:13:55 +0100

    Create Box component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import Box from './components/Box/Box';
+
 import './App.css';
 
 const App = () => (
   <div className="App">
     <Box>
+    </Box>
+  </div>
 );
 
 export default App;
