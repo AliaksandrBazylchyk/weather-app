4b211f70de254c8e87823fbad7db2d31161ffdef
Aliaksandr Bazylchyk <bazylchyk.a@gmail.com>
2022-12-29 12:38:06 +0100

    Create daily weather

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react';
 
+import { getLocation } from './services/location.service';
+import { getDailyWeather } from './services/weather.service';
+
+import { DailyWeather } from './types/dailyWeatherInterface';
+
 import Box from './components/Box/Box';
 
 import './App.css';
@@ -9,31 +14,41 @@ const App = () => {
   const [lng, setLng] = useState<number | null>(null);
   const [status, setStatus] = useState<string | null>(null);
 
-  const getLocation = async () => {
-    if (!navigator.geolocation) {
-      setStatus('Is not supported by your browser!');
-    } else {
-      setStatus('Locating...');
-      navigator.geolocation.getCurrentPosition((position) => {
-        setStatus(null);
-        setLat(position.coords.latitude);
-        setLng(position.coords.longitude);
-      }, () => {
-        setStatus('Unable to retrieve your location.');
-      });
-    }
-  };
+  const [dailyWeather, setDailyWeather] = useState<DailyWeather | null>(null);
 
   useEffect(() => {
-    getLocation();
+    getLocation(setLat, setLng, setStatus);
   }, []);
+
+  useEffect(() => {
+    const daily = localStorage.getItem('daily');
+    if (lat && lng && !daily) {
+      getDailyWeather(lat, lng, setDailyWeather);
+    } else if (daily) {
+      setDailyWeather(JSON.parse(daily));
+    }
+  }, [lat, lng]);
 
   return (
     <div className="App">
       <Box>
         <div className="calendar-panel" />
         <div className="weather-panel">
-          <div className="weather-panel-today">Ha-ha</div>
+          <div className="weather-panel-today">
+            <div className="today-icon">
+              <img
+                src={`${process.env.REACT_APP_OPEN_WEATHER_ICONS_ENDPOINT}/${dailyWeather?.weather[0].icon}@2x.png`}
+                alt={`${dailyWeather?.weather[0].main}`}
+              />
+            </div>
+            <div className="today-info">
+              <span className="text-title">Today</span>
+              <span className="today-temperature">
+                {dailyWeather?.main.temp}
+                &#176;
+              </span>
+            </div>
+          </div>
           <div className="weather-panel-weekly">
             {status}
             Your lat:
